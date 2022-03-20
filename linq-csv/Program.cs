0dda1b51863e6b7bba6e6609c87bae767e557e9c
Krzysztof Żmijewski <kzmijewski@protonmail.com>
2022-03-20 18:50:37 +0100

    Add project files.

---
+++ linq-csv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linq_csv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
